2691fa00fbf07b6d3c7e9141dca05537fd2a7f8c
gotop1711 <gotop1711@gmail.com>
2025-01-14 21:21:12 +0800

    fix: js bugs

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        const sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
@@ -47,7 +47,7 @@ window.addEventListener('scroll', scrollActive)
 function scrollTop(){
   const scrollTop = document.getElementById('scroll-top');
   // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+  if(window.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
